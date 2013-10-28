70c1e14acafd97b2128af5eeea313e4202953ecc
aevange <andre.evangelesta@gmail.com>
2013-10-28 10:21:24 -0700

    Change D3 source to CDN for performance

--- scripts/services/services.js
+++ scripts/services/services.js
@@ -12,7 +12,7 @@ angular.module('roots.services', [])
       var scriptTag = $document[0].createElement('script');
       scriptTag.type = 'text/javascript'; 
       scriptTag.async = true;
-      scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+      scriptTag.src = 'http://cdnjs.cloudflare.com/ajax/libs/d3/3.3.3/d3.min.js';
       scriptTag.onreadystatechange = function () {
         if (this.readyState == 'complete') onScriptLoad();
       }
